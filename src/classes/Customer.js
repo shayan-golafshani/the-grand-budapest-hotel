5a2381902677c38c86a3ebf90b611252214247c8
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-11 13:06:36 -0600

    Add methods and some pseudo code for the customer class

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,17 +1,28 @@
 class Customer {
   constructor(bookingInfo, lifeTimeSpending) {
-
+    //properties
     ///this is an array of booking objects with all the info needed
-    this.bookings = bookingInfo || 0;
+    this.bookings = bookingInfo || [];
 
     //this is the the total amount that a customer has spent in their lifetime at the GBH
     this.totalSpent = lifeTimeSpending || 0;
 
-    this.nextBooking = 
+    this.nextBooking = new Date () || "Jan. 1st, 2100";
   }
 
-    
+    //methods
+    selectNextBookableData() {
+        //should update this.nextBooking
 
+    }
+
+    getAvailableRoomDetails() {
+        //should be shown a list of room details for only rooms that are available on that date
+
+        //need to check the boolean status of whether or not the room in question is available.
+
+        //return an array of room details....for rooms available on that date
+    }
 
 
 
