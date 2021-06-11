20dbde79e26f30827afbb760ed9581c89384b748
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-11 12:21:59 -0600

    Start building out the customer class

---
+++ src/classes/Customer.js
@@ -0,0 +1,23 @@
+class Customer {
+  constructor(bookingInfo, lifeTimeSpending) {
+
+    ///this is an array of booking objects with all the info needed
+    this.bookings = bookingInfo || 0;
+
+    //this is the the total amount that a customer has spent in their lifetime at the GBH
+    this.totalSpent = lifeTimeSpending || 0;
+
+    this.nextBooking = 
+  }
+
+    
+
+
+
+
+
+
+
+}
+
+module.exports = Customer; 
