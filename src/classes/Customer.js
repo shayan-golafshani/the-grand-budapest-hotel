5753c5f5eae5d1a10bbb5b5a2bc8d2d6800967ff
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 16:29:54 -0600

    Restructure customer after speaking with Demaceo

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,20 +1,15 @@
 class Customer {
-  constructor(bookingInfo, lifeTimeSpending, id, name) {
+  constructor(id, name) {
     //properties
-    ///this is an array of booking objects with all the info needed
-    this.bookings = bookingInfo || [];
-
-    this.nextBooking = new Date () || "Jan. 1st, 2100";
-    
-    //this is the the total amount that a customer has spent in their lifetime at the GBH
-    this.totalSpent = lifeTimeSpending || 0;
-
     //customer id
     this.id = id;
     //customer's name
     this.name = name;
+    //this is the the total amount that a customer has spent in their lifetime at the GBH
+    this.totalSpent = 0;
+    //this is an array of booking objects with all the customer's past info
+    this.bookings = [];  
   }
-
     //methods
     selectNextBookableData() {
         //should update this.nextBooking
@@ -29,7 +24,18 @@ class Customer {
         //return an array of room details....for rooms available on that date
     }
 
+    filter
 
+/*
+I should be able to select a date for which I’d like to book a room for myself
+Upon selecting a date, I should be shown a list of room details for only rooms that are available on that date
+I should be able to filter the list of available rooms by their roomType property
+I should be able to select a room for booking
+In the event that no rooms are available for the date/roomType selected, display a message fiercely apologizing to the user and asking them to adjust their room search
+
+
+
+*/
 
 
 
