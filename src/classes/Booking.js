598ec8def87b00efbe7d1f797c9c61f82293a3f2
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 17:17:11 -0600

    Add Booking class

---
+++ src/classes/Booking.js
@@ -0,0 +1,9 @@
+export class Booking {
+  constructor(bookingInfo) {
+    this.id = bookingInfo.id;
+    this.userID = bookingInfo.userID;
+    this.date = bookingInfo.date;
+    this.roomNumber = bookingInfo.roomNumber;
+    this.roomServiceCharges = bookingInfo.roomServiceCharges || [];
+  }
+}
