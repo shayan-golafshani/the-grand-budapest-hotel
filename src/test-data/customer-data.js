4915a20f7624322c6e0cc2399b7556d6bfff355f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 13:34:21 -0600

    Add sample guest data

---
+++ src/test-data/customer-data.js
@@ -0,0 +1,42 @@
+export let customers = [
+  {
+    id: 1,
+    name: "Lil Wayne"
+  },
+  {
+    id: 2,
+    name: "Bob Marley"
+  },
+  {
+    id: 3,
+    name: "Marshall Mathers"
+  },
+  {
+    id: 4,
+    name: "Shayan Golafshani"
+  },
+  {
+    id: 5,
+    name: "Nik Seif"
+  },
+  {
+    id: 6,
+    name: "Andrew Vallejo"
+  },
+  {
+    id: 7,
+    name: "Tom Brady"
+  },
+  {
+    id: 8,
+    name: "Scarlett Johannson"
+  },
+  {
+    id: 9,
+    name: "Eddie Murphy"
+  },
+  {
+    id: 10,
+    name: "Dave Chappelle"
+  }
+]
