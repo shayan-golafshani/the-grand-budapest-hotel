3cbf85348a1f2cca680314b348658aaea1f522e5
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 16:33:06 -0600

    Make allCustomers only accessible for less confusion

--- src/test-data/customer-data.js
+++ src/test-data/customer-data.js
@@ -1,45 +1,45 @@
-export let testCustomers = [
-  {
-    id: 1,
-    name: "Lil Wayne"
-  },
-  {
-    id: 2,
-    name: "Bob Marley"
-  },
-  {
-    id: 3,
-    name: "Marshall Mathers"
-  },
-  {
-    id: 4,
-    name: "Shayan Golafshani"
-  },
-  {
-    id: 5,
-    name: "Nik Seif"
-  },
-  {
-    id: 6,
-    name: "Andrew Vallejo"
-  },
-  {
-    id: 7,
-    name: "Tom Brady"
-  },
-  {
-    id: 8,
-    name: "Scarlett Johannson"
-  },
-  {
-    id: 9,
-    name: "Eddie Murphy"
-  },
-  {
-    id: 10,
-    name: "Dave Chappelle"
-  }
-]
+// export const testCustomers = [
+//   {
+//     id: 1,
+//     name: "Lil Wayne"
+//   },
+//   {
+//     id: 2,
+//     name: "Bob Marley"
+//   },
+//   {
+//     id: 3,
+//     name: "Marshall Mathers"
+//   },
+//   {
+//     id: 4,
+//     name: "Shayan Golafshani"
+//   },
+//   {
+//     id: 5,
+//     name: "Nik Seif"
+//   },
+//   {
+//     id: 6,
+//     name: "Andrew Vallejo"
+//   },
+//   {
+//     id: 7,
+//     name: "Tom Brady"
+//   },
+//   {
+//     id: 8,
+//     name: "Scarlett Johannson"
+//   },
+//   {
+//     id: 9,
+//     name: "Eddie Murphy"
+//   },
+//   {
+//     id: 10,
+//     name: "Dave Chappelle"
+//   }
+// ]
 
 export const allCustomers = [
   {
