df261aca9233ca23d9f1073757aa15193c5ab2c9
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 16:30:11 -0600

    Create star for tests

---
+++ test/customer-test.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Customer from '../src/classes/Customer';
+import customers from '../src/test-data/customer-data';
+import bookings from '../src/data/sampleBookingsData';
+import sampleRoomsData from '../src/data/sampleRoomsData';
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+  it('should be able to select a date to book a room for myself', () => {
+
+
+
+    expect(true).to.equal(true);
+  });
+
+  it('should return a list of room details for rooms available on only the booking date', () => {
+
+
+
+    expect(true).to.equal(true);
+  });
+});
