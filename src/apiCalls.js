18362d145e6c3de5f0bd0dcc1c9acdfc9a197ee6
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 11:29:29 -0600

    Complete Basic Setup of all ApiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,27 @@
-let getData = (endpoint) => fetch(`http://localhost:3001/api/v1/${endpoint}`)
-  .then(response => response.json())
-  .catch(err => console.log('error1', err));
+let baseUrl = 'http://localhost:3001/api/v1/'
 
-function retrieveData() {
-  return Promise.all([getData('users'), getData('hydration'), getData('sleep'), getData('activity')])
+//***********************************ALL GET RELATED INFO ******************/
+
+let getData = (endpoint, id) => {
+  let url = (id) ? `${baseUrl}${endpoint}/${id}` : `${baseUrl}${endpoint}`;    
+  fetch(url)
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .catch(err => console.log('Get didnt work!', err))
+};
+
+//Make sure to double check the getData running with the userId, I can't run this whole method, unless I 
+// I have the users Id at this point meaning the last two chars spliced off the end of the userInput on the login.
+function retrieveData(userId) {
+  return Promise.all([getData('customers'), getData('customers', userId), getData('rooms'), getData('bookings')])
 }
+//***********************************ALL GET RELATED INFO ******************/
+
+//***********************************ALL POSTS RELATED INFO ******************/
+
+let bookingPost  = {"userID": 48, "date": "2019/09/23", "roomNumber": 4, } // an example of what would be passed through sentData
 
 let sendData = (sentData, url) => {
   return fetch(url, {
@@ -15,12 +32,27 @@ let sendData = (sentData, url) => {
     }
   })
     .then(response => response.json())
-    .then(json => console.log("This is the Json post being sent from the API FILE", json))
+    .then(json => console.log("Json post being sent from the apiCalls.js", json))
     .catch(err => console.log(err));
 }
 
-let postData = (postableData, urlEndpoint) => {
-  return Promise.all([sendData(postableData, `http://localhost:3001/api/v1/${urlEndpoint}`)]);
+//example postcall would be postData(bookingPost);
+let postData = (postableData) => {
+  return Promise.all([sendData(postableData, `${baseUrl}bookings`)]);
 }
+//***********************************ALL POSTS RELATED INFO ******************/
 
-export {retrieveData, postData};
+//***********************************ALL DELETE RELATED INFO ******************/
+let deleteBooking = (bookingId) => {
+  const deleteMethod = {
+    method: 'DELETE',
+    headers: {'Content-type': 'application/json'},
+  }      
+  return fetch(`${baseUrl}/bookings/${bookingId}`, deleteMethod) 
+    .then(response => response.json())
+    .then(data => console.log(data)) 
+    .catch(err => console.log(err))
+}
+//***********************************ALL DELETE RELATED INFO ******************/
+
+export {retrieveData, postData, deleteBooking};
