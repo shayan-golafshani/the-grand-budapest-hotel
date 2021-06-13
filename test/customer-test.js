634702f57d34f83ac3a58392db68adfd5042bf38
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 20:29:01 -0600

    Attempt implementing customer test first round

--- test/customer-test.js
+++ test/customer-test.js
@@ -2,26 +2,38 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import Customer from '../src/classes/Customer';
-import customers from '../src/test-data/customer-data';
-import bookings from '../src/data/sampleBookingsData';
-import sampleRoomsData from '../src/data/sampleRoomsData';
+import { allCustomers } from '../src/test-data/customer-data';
+import { bookings } from '../src/test-data/booking-data';
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe.only('Customer', () => {
+  let customer1;
+  beforeEach(() => {
+
+    customer1 = new Customer(allCustomers[0]);
   });
 
-  it('should be able to select a date to book a room for myself', () => {
-
-
-
-    expect(true).to.equal(true);
+  it('Should be a function ', () => {
+    expect(Customer).to.be.a('function');
   });
 
-  it('should return a list of room details for rooms available on only the booking date', () => {
+  it('Should have an id and a name', () => {
+    expect(customer1.id).to.equal(allCustomers[0].id);
+    expect(customer1.name).to.equal(allCustomers[0].name);
+  });
 
+  it('Should have a default totalSpent of 0, until tallied', () => {
+    expect(customer1.totalSpent).to.equal(0);
+  });
 
+  it('Should have an empty list of bookings by default', () => {
+    expect(customer1.bookings.length).to.equal(0);
+    expect(customer1.bookings).to.deep.equal([]);
+  });
 
-    expect(true).to.equal(true);
+  it('Should return a list of room numbers for rooms that are available on only the booking date', () => {
+    const roomNums = customer1.filterRoomAvailabilityByDate("2020/02/05", bookings)
+    //room number 12 should be excluded from the list
+    const expectedRoomNums = [15, 24, 7, 14, 9, 5, 13, 20, 8, 2, 17]; 
+    expect(roomNums).to.equal(expectedRoomNums);
   });
 });
