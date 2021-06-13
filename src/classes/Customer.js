a617403d14a1e072548e56ad039cd74af683c1a7
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-12 22:49:21 -0600

    Finish filterRoomsByType

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -23,17 +23,21 @@ class Customer {
       room => this.availableRoomNums.includes(room.number));
   }
 
+  filterRoomsByRoomType(rooms, roomType) {
+    let availableRooms = this.getAvailableRoomDetails(rooms);
+
+    let filteredByType = availableRooms.filter(roomObject => roomObject.roomType === roomType);
+    //probably don't actually need the room nums, but I could map over
+    //filteredByType and then actually try and return just the room.number
+
+    console.log('This is your filteredRooms*****', filteredByType)
+    return filteredByType; 
+  }
+  
+
   /*
-    //should be shown a list of room details for only rooms that are available on that date
 
-    //need to check the boolean status of whether or not the room in question is available.
 
-    //return an array of room details....for rooms available on that date
-   */
-
-  /*
-I should be able to select a date for which I’d like to book a room for myself
-Upon selecting a date, I should be shown a list of room details for only rooms that are available on that date
 I should be able to filter the list of available rooms by their roomType property
 I should be able to select a room for booking
 In the event that no rooms are available for the date/roomType selected, display a message fiercely apologizing to the user and asking them to adjust their room search
