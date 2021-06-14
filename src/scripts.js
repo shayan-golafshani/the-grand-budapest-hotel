af85dcffe1a33f7b0f9265cc8a977f6aebffcc61
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-13 23:26:21 -0600

    Update to the dom to show past bookings, names,  and dates booked , and the total customer amount spent at the hotel!

--- src/scripts.js
+++ src/scripts.js
@@ -8,5 +8,39 @@ import './css/base.scss';
 import './images/turing-logo.png'
 import './images/hungary.png'
 import './images/the-grand-budapest-hotel.png'
+import { retrieveData } from './apiCalls';
+import Customer from './classes/Customer';
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+//Put query-selectors here
+let totalSpent = document.getElementById('totalSpent');
+let guestName = document.getElementById('guestName');
+let roomSelectionForm = document.getElementById('roomSelectionForm');
+//put event listeners on here. 
+
+window.onload = startUp;
+let startUpArr = [];
+
+function startUp() {
+  retrieveData(1)
+    .then(promise => {
+      let customers = promise[0];
+      let currCustomer = promise[1];
+      let rooms = promise[2];
+      let bookings = promise[3];
+      startUpArr = [customers, currCustomer, rooms, bookings];
+      console.log(promise);
+      currCustomer = new Customer(currCustomer);
+      guestName.innerText = currCustomer.name;
+      totalSpent.innerText = currCustomer.viewCustomerTotalSpending(bookings.bookings, rooms.rooms);
+      currCustomer.bookings.forEach(booking => {
+        roomSelectionForm.innerHTML += `
+        <p> Room number ${booking.roomNumber}</p>
+        <p>Booked on ${booking.date}</p>
+        `    
+      });
+    })//.catch(err => //console.error("Error is happening scripts", err));
+//totalSpent.innerText = "Bobby and Shauna rock" + ;
+return true;
+}
